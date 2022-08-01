318aa7418911f996d716b9950f98f251db8499f1
vrvimal <vrvprasath@gmail.com>
2022-08-01 12:54:04 -0400

    fixed issue

--- SBA/JS/Register.js
+++ SBA/JS/Register.js
@@ -12,6 +12,11 @@ function register(){
 }
 function checkInput(){
     const details = [];
+    let formValid = false;
+    let userNameValid=false;
+    let emailValid=false;
+    let passwordValid=false;
+    let confirmPasswordValid=false;
     details.push(userName.value.trim());
     details.push(email.value.trim());
     details.push(password.value.trim());
@@ -21,6 +26,7 @@ function checkInput(){
         setError(userName,'Username cannot be blank');
     }else{
         setSuccess(userName);
+        userNameValid=true;
     }
     if (details[1] == ''){
         setError(email,'email cannot be blank');
@@ -29,11 +35,13 @@ function checkInput(){
     }
     else{
         setSuccess(email);
+        emailValid=true;
     }
     if (details[2] == ''){
         setError(password,'password cannot be blank');
     }else{
         setSuccess(password);
+        passwordValid=true;
     }
     if (details[3] == ''){
         setError(passwordCheck,'passwordCheck cannot be blank');
@@ -42,6 +50,7 @@ function checkInput(){
     }
     else{
         setSuccess(passwordCheck);
+        confirmPasswordValid=true;
     }
 }
 
@@ -62,4 +71,11 @@ function setError(input, msg){
 function setSuccess(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+
+}
+
+function formValidate(){
+    if ( confirmPasswordValid && passwordValid && emailValid && userNameValid ){
+        formValid = true;
+    }
 }
